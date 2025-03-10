e3bcef5c6e03ef3bcf341edc81b79a5c52da57bd
DELL <DELL@DESKTOP-TT8I15K>
2025-03-10 12:41:23 +0530

    #Multiplication scenario

--- Pythonbasic.py
+++ Pythonbasic.py
@@ -1,4 +1,7 @@
 # Sum of two numbers
+from tkinter import MULTIPLE
+
+
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
@@ -9,3 +12,7 @@ print("The sum of", num1, "and", num2, "is:", sum)
 # Average
 average = sum/2
 print("The avg of two number is:", average)
+
+# Multiply
+MULTIPLE = num1 * num2
+print("The multiply value of two number is:", MULTIPLE)
